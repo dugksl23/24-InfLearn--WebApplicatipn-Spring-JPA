d13b4acace5d98c1500c6ceecc620fccddcca15a
dugksl23 <dugksl23@gmail.com>
2024-04-13 14:20:47 +0900

    queryDsl 적용 및 동적 쿼리 생성

--- src/main/java/JPA/Book/jpashop/order/repository/OrderRepository.java
+++ src/main/java/JPA/Book/jpashop/order/repository/OrderRepository.java
@@ -1,21 +1,28 @@
 package JPA.Book.jpashop.order.repository;
 
-
+import JPA.Book.jpashop.Member.domain.QMember;
 import JPA.Book.jpashop.order.domain.Order;
+import JPA.Book.jpashop.order.domain.OrderSearch;
+import JPA.Book.jpashop.order.domain.OrderStatus;
+import JPA.Book.jpashop.order.domain.QOrder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class OrderRepository {
 
     private final EntityManager em;
+    private JPAQueryFactory query;
 
     public Long saveOrder(Order order) {
         em.persist(order);
@@ -27,12 +34,42 @@ public class OrderRepository {
         return order;
     }
 
-    public List<Order> findAllOrders() {
-        return em.createQuery("select 0 from Order o", Order.class).getResultList();
+    public List<Order> findAllOrders(OrderSearch orderSearch) {
+
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+
+    }
+    // JPQL Join 문 ex)　select o from Order o left join Member m
+
+    public List<Order> SearchOrder(String keyword) {
+        return new ArrayList<>();
     }
 
-    public List<Order> SearchOrder (String keyword) {
-        return new ArrayList<>();
+    private BooleanExpression statusEq(OrderStatus statusCond){
+        QOrder order = QOrder.order;
+
+        if (statusCond == null){
+            return null;
+        }
+        return order.orderStatus.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String nameCond){
+        QMember member = QMember.member;
+        if (!StringUtils.hasText(nameCond)){
+            return null;
+        }
+        return member.name.like(nameCond);
     }
 
 
